PX4/PX4-Autopilot

Omni Pos-Ctrl: Reduced jerking when a  large command is given in offboard
- OMNI_ATT_MODE = 2 uses OMNI_DFC_MAX_THR parameter to limit the horizontal commanded thrust now
- Other modes are not affected

src/modules/mc_pos_control/PositionControl/PositionControl.cpp
@@ -89,7 +89,7 @@ bool PositionControl::setInputSetpoint(const vehicle_local_position_setpoint_s &
 	return mapping_succeeded;
 }
 
-void PositionControl::setConstraints(const vehicle_constraints_s &constraints)
+void PositionControl::setConstraints(const vehicle_constraints_s &constraints, const float lim_thr_hor_max)
 {
 	_constraints = constraints;
 
@@ -106,6 +106,8 @@ void PositionControl::setConstraints(const vehicle_constraints_s &constraints)
 	if (!PX4_ISFINITE(constraints.speed_down) || (constraints.speed_down > _lim_vel_down)) {
 		_constraints.speed_down = _lim_vel_down;
 	}
+
+	_lim_thr_hor_max = lim_thr_hor_max;
 
 	// ignore _constraints.speed_xy TODO: remove it completely as soon as no task uses it anymore to avoid confusion
 }
@@ -321,6 +323,7 @@ void PositionControl::_velocityControl(const float dt)
 		float thrust_max_NE_tilt = fabsf(_thr_sp(2)) * tanf(_constraints.tilt);
 		float thrust_max_NE = sqrtf(_lim_thr_max * _lim_thr_max - _thr_sp(2) * _thr_sp(2));
 		thrust_max_NE = math::min(thrust_max_NE_tilt, thrust_max_NE);
+		thrust_max_NE = math::min(_lim_thr_hor_max, thrust_max_NE);
 
 		// Saturate thrust in NE-direction.
 		_thr_sp(0) = thrust_desired_NE(0);

asaba96/Firmware

src/modules/mc_pos_control/PositionControl/PositionControl.cpp
@@ -88,7 +88,7 @@ void PositionControl::setInputSetpoint(const vehicle_local_position_setpoint_s &
 	_yawspeed_sp = setpoint.yawspeed;
 }
 
-void PositionControl::setConstraints(const vehicle_constraints_s &constraints)
+void PositionControl::setConstraints(const vehicle_constraints_s &constraints, const float lim_thr_hor_max)
 {
 	_constraints = constraints;
 
@@ -105,6 +105,8 @@ void PositionControl::setConstraints(const vehicle_constraints_s &constraints)
 	if (!PX4_ISFINITE(constraints.speed_down) || (constraints.speed_down > _lim_vel_down)) {
 		_constraints.speed_down = _lim_vel_down;
 	}
+
+	_lim_thr_hor_max = lim_thr_hor_max;
 
 	// ignore _constraints.speed_xy TODO: remove it completely as soon as no task uses it anymore to avoid confusion
 }
@@ -165,6 +167,7 @@ void PositionControl::_velocityControl(const float dt)
 	const float thrust_max_squared = _lim_thr_max * _lim_thr_max;
 	const float thrust_z_squared = _thr_sp(2) * _thr_sp(2);
 	float thrust_max_xy = sqrtf(thrust_max_squared - thrust_z_squared);
+	thrust_max_xy = math::min(_lim_thr_hor_max, thrust_max_xy);
 
 	// Saturate thrust in horizontal direction
 	const Vector2f thrust_sp_xy(_thr_sp);